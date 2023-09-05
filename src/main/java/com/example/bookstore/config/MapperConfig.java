0d9c89e3c9b25dd7a9478f92ebfc25dfda0a4ffd
Ihor <kalaurdelta96@gmail.com>
2023-09-05 23:22:18 +0300

    changed path to package in implementationPackage property

--- src/main/java/com/example/bookstore/config/MapperConfig.java
+++ src/main/java/com/example/bookstore/config/MapperConfig.java
@@ -7,7 +7,7 @@ import org.mapstruct.NullValueCheckStrategy;
         componentModel = "spring",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
         nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
-        implementationPackage = "com.example.bookstore.mapper.impl"
+        implementationPackage = "<PACKAGE_NAME>.impl"
 )
 public class MapperConfig {
 }
