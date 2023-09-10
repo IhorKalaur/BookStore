89e7ee8160e2dbf3c19bd290c4b150382e4822a0
Ihor <kalaurdelta96@gmail.com>
2023-09-10 13:31:20 +0300

    added user and roles model

---
+++ src/main/java/com/example/bookstore/model/User.java
@@ -0,0 +1,60 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.Set;
+import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Data
+@Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email",
+            nullable = false,
+            unique = true)
+    private String email;
+
+    @Column(name = "password",
+            nullable = false)
+    private String password;
+
+    @Column(name = "first_name",
+            nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name",
+            nullable = false)
+    private String lastName;
+
+    @Column(name = "shipping_address ")
+    private String shippingAddress;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    @Column(name = "is_deleted",
+            nullable = false)
+    private boolean isDeleted = false;
+}
