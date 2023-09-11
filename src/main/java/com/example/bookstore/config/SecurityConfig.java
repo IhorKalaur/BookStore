5cf292d5dff8deacf58113a883e118d603b784ff
Ihor <kalaurdelta96@gmail.com>
2023-09-11 15:05:28 +0300

    test commit

--- src/main/java/com/example/bookstore/config/SecurityConfig.java
+++ src/main/java/com/example/bookstore/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -37,7 +38,8 @@ public class SecurityConfig {
 
                 )
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .userDetailsService(userDetailsService)
                 .build();
     }
