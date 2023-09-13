1ecfb27fc72cd901fe96d99b9d3f6f8fa058bb2c
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-09-13 22:28:29 +0300

    create category entity and table

---
+++ src/main/java/com/example/bookstore/model/Category.java
@@ -0,0 +1,24 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+    @Column(name = "name",
+            nullable = false)
+    String name;
+    @Column(name = "description")
+    String description;
+}
