b8a1041afddf31ea38de038fc9fbcef4980461ef
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-09-13 19:12:59 +0300

    try to fix conflict

--- src/main/java/com/example/bookstore/service/BookService.java
+++ src/main/java/com/example/bookstore/service/BookService.java
@@ -2,11 +2,12 @@ package com.example.bookstore.service;
 
 import com.example.bookstore.dto.book.BookDto;
 import com.example.bookstore.dto.book.CreateBookRequestDto;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 
 public interface BookService {
-    BookDto save(CreateBookRequestDto requestDtoBook);
+    BookDto save(@Valid CreateBookRequestDto requestDtoBook);
 
     List<BookDto> findAll(Pageable pageable);
 
