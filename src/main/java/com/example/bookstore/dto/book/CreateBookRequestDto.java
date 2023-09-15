156fe276b95932a0432b65529446c66608d494e9
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-09-15 14:41:14 +0300

    added empty to pom.xml

--- src/main/java/com/example/bookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/book/CreateBookRequestDto.java
@@ -12,12 +12,13 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class CreateBookRequestDto {
     @NotBlank(message = "Can't be null or empty")
-    @Length(min = 4, max = 50, message = "Length must be more than  4, but less than  50")
-    @Pattern(regexp = "^[a-zA-Z0-9\\s]+$", message = "Only Latin letters and digits are allowed")
+    @Length(min = 3, max = 50, message = "Length must be more than  3, but less than  50")
+    @Pattern(regexp = "^[a-zA-Z0-9\\s.,'-]+$",
+            message = "Only Latin letters, digits, and special characters are allowed")
     private String title;
     @NotBlank(message = "Can't be null or empty")
     @Length(min = 5, max = 50, message = "Length must be more than  5, but less than  50")
-    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "Only Latin letters are allowed")
+    @Pattern(regexp = "^[A-Za-z\\s']+$", message = "Only Latin letters are allowed")
     private String author;
     @NotNull
     @Isbn
