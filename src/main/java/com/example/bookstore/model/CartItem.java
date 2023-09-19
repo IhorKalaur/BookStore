5e5c9bcb3b99d1b83f84829ab9c3d6c2a63bf7bd
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-09-19 03:00:37 +0300

    added is_delete column, create changeSet for this

--- src/main/java/com/example/bookstore/model/CartItem.java
+++ src/main/java/com/example/bookstore/model/CartItem.java
@@ -10,9 +10,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE cart_items SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "cart_items")
 public class CartItem {
     @Id
@@ -34,4 +38,8 @@ public class CartItem {
     @Column(name = "quantity",
             nullable = false)
     private Integer quantity;
+
+    @Column(name = "is_deleted",
+            nullable = false)
+    private boolean isDeleted = false;
 }
--- src/main/java/com/example/bookstore/model/ShoppingCart.java
+++ src/main/java/com/example/bookstore/model/ShoppingCart.java
@@ -15,9 +15,13 @@ import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.apache.commons.lang3.builder.ToStringExclude;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
@@ -33,4 +37,8 @@ public class ShoppingCart {
     @ToStringExclude
     @EqualsAndHashCode.Exclude
     private Set<CartItem> cartItems = new HashSet<>();
+
+    @Column(name = "is_deleted",
+            nullable = false)
+    private boolean isDeleted = false;
 }
